2c9d40ace2ca90c2f56b0108dee363015f0f5bf0
Syed Hassan Shah <152647051+SyedHassanShah8004086@users.noreply.github.com>
2025-02-14 18:34:43 +0500

    Add files via upload

---
+++ Table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"Enter any no for Table : ";
+	cin>>a;
+	cout<<"Enter the Range of Table : ";
+	cin>>b;
+	for(int i=1;i<=b;i++){
+		cout<<a<<"\tx\t"<<i<<"\t=\t"<<a*i<<endl;
+	}
+}
